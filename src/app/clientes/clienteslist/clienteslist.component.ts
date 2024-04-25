ea3ccae9dd11c3f9d215516c9a51b0b20b104cfd
Henrique <henry2camargo@gmail.com>
2024-04-25 15:55:29 -0300

    Comclusão da tela de cliente

--- src/app/clientes/clienteslist/clienteslist.component.ts
+++ src/app/clientes/clienteslist/clienteslist.component.ts
@@ -9,25 +9,38 @@ import { Router } from '@angular/router';
   styleUrls: ['./clienteslist.component.css']
 })
 export class ClienteslistComponent implements OnInit {
-preparaDelecao(_t23: Cliente) {
-throw new Error('Method not implemented.');
-}
-
-voltarParaListagem() {
-throw new Error('Method not implemented.');
-}
 
   Clientes: Cliente[] = [];
-
+  clienteSelecionado: Cliente;
+  mensagemSucesso: string;
+  mensagemErro: string;
+  
   constructor(private service: ClientesService, private router: Router){ }
-
+  
   ngOnInit(): void{
-     this.service
-     .getClientes()
-     .subscribe( respota => this.Clientes = respota);
+    this.service
+    .getClientes()
+    .subscribe( respota => this.Clientes = respota);
   }
 
   novoCadastro() {
     this.router.navigate(['/clientes-form'])
-    }
+  }
+  
+  preparaDelecao(cliente: Cliente){
+    this.clienteSelecionado = cliente;
+  }
+  
+  deletarCliente(){
+    this.service
+      .deletar(this.clienteSelecionado)
+      .subscribe( 
+        response => {
+          this.mensagemSucesso = 'Cliente deletado com sucesso!'
+          this.ngOnInit();
+        },
+        erro => this.mensagemErro = 'Ocorreu um erro ao deletar o cliente.'  
+      )
+  }
 }
+
